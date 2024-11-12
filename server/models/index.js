22b4123df18a0f83b0f8a87038ae5ddb011325df
Ownerman123 <charlesshumway1@gmail.com>
2024-11-12 10:40:26 -0700

    whoops wierd how that still worked locally

--- server/models/index.js
+++ server/models/index.js
@@ -3,7 +3,7 @@ import User from "./User.js";
 import Recipe from "./Recipe.js";
 import RefreshToken from "./RefreshToken.js";
 import Tag from "./Tag.js";
-import Ingredient from "./ingredient.js";
+import Ingredient from "./Ingredient.js";
 
 
 export { User, Recipe, RefreshToken, Tag, Ingredient };
